b203f93faa159ed94120f517d839b604051d6d14
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2020-06-09 19:56:57 +0200

    update

--- agents/REPTILE.py
+++ agents/REPTILE.py
@@ -11,15 +11,18 @@ class REPTILE(nn.Module):
     def __init__(self, config):
         super(REPTILE, self).__init__()
 
-        reptile_config = config['agents']['reptile']
-        self.max_iterations = reptile_config['max_iterations']
-        self.step_size = reptile_config['step_size']
-        self.agent_name = reptile_config['agent_name']
+        reptile_config = config["agents"]["reptile"]
+        self.max_iterations = reptile_config["max_iterations"]
+        self.step_size = reptile_config["step_size"]
+        self.agent_name = reptile_config["agent_name"]
 
         self.env_factory = EnvFactory(config)
         self.agent = self.agent_factory(config)
-        self.seeds = torch.tensor(np.random.choice(range(self.max_iterations), self.max_iterations, replace=False),
-                                  device='cpu', dtype=torch.float32).unsqueeze(1)
+        self.seeds = torch.tensor(
+            np.random.choice(range(self.max_iterations), self.max_iterations, replace=False),
+            device="cpu",
+            dtype=torch.float32,
+        ).unsqueeze(1)
 
     def run(self):
         for it in range(self.max_iterations):
@@ -29,43 +32,19 @@ class REPTILE(nn.Module):
             self.agent.run(env=env)
             new_state_dict = self.agent.state_dict()
 
-            # print('old')
-            # print(old_state_dict['actor.action_std'])
-            # print(new_state_dict['actor.action_std'])
-            # print(self.agent.state_dict()['actor.action_std'])
-
-            for key,value in new_state_dict.items():
-                new_state_dict[key] = old_state_dict[key] \
-                                      + (new_state_dict[key] - old_state_dict[key]) * self.step_size
+            for key, value in new_state_dict.items():
+                new_state_dict[key] = old_state_dict[key] + (new_state_dict[key] - old_state_dict[key]) * self.step_size
 
             self.agent.load_state_dict(new_state_dict)
-
-            # print('new')
-            # print(old_state_dict['actor.action_std'])
-            # print(new_state_dict['actor.action_std'])
-            # print(self.agent.state_dict()['actor.action_std'])
-            #
-            # self.agent.load_state_dict(new_state_dict)
-            #
-            # print('newer')
-            # print(old_state_dict['actor.action_std'])
-            # print(new_state_dict['actor.action_std'])
-            # print(self.agent.state_dict()['actor.action_std'])
-
 
     def agent_factory(self, config):
         dummy_env = self.env_factory.generate_default_real_env()
         state_dim = dummy_env.observation_space.shape[0]
         action_dim = dummy_env.action_space.shape[0]
 
-        if self.agent_name == 'TD3':
+        if self.agent_name == "TD3":
             return TD3(state_dim, action_dim, config)
-        elif self.agent_name == 'PPO':
+        elif self.agent_name == "PPO":
             return PPO(state_dim, action_dim, config)
         else:
-            raise NotImplementedError('Unknownn RL agent')
-
-
-
-
-
+            raise NotImplementedError("Unknownn RL agent")
--- envs/env_factory.py
+++ envs/env_factory.py
@@ -1,7 +1,6 @@
+import torch
 import gym
 import numpy as np
-import torch
-
 from envs.pendulum_env import PendulumEnv
 from envs.virtual_env import VirtualEnv
 
@@ -23,7 +22,7 @@ class EnvWrapper(gym.Wrapper):
             self.state = args[1]
             return self._step(args[0])
         else:
-            raise NotImplementedError('Unknown number of input arguments')
+            raise NotImplementedError("Unknown number of input arguments")
 
     def _step(self, action):
         return super().step(action)
@@ -34,9 +33,7 @@ class EnvWrapper(gym.Wrapper):
     def random_action(self):
         # do random action in the [-1,1] range
         # TODO: should be modified if environment has different range
-        return torch.empty(self.get_action_dim(),
-                           device='cpu',
-                           dtype=torch.float32).uniform_(-1, 1)
+        return torch.empty(self.get_action_dim(), device="cpu", dtype=torch.float32).uniform_(-1, 1)
 
     def get_state_dim(self):
         if self.is_virtual_env:
@@ -53,11 +50,11 @@ class EnvWrapper(gym.Wrapper):
 
 class EnvFactory:
     def __init__(self, config):
-        self.env_name = config['env_name']
-        self.seed = config['seed']
+        self.env_name = config["env_name"]
+        self.seed = config["seed"]
         self.env_config = {}
-        if self.env_name in config['envs'].keys():
-            self.env_config = config['envs'][self.env_name]
+        if self.env_name in config["envs"].keys():
+            self.env_config = config["envs"][self.env_name]
 
         dummy_env = self.generate_default_real_env()
         self.state_dim = dummy_env.observation_space.shape[0]
@@ -66,33 +63,27 @@ class EnvFactory:
     def generate_default_real_env(self):
         # generate a real environment with default parameters
         kwargs = self._get_default_parameters()
-        print('Generating default real environment "{}" with parameters {}'.
-              format(self.env_name, kwargs))
+        print('Generating default real environment "{}" with parameters {}'.format(self.env_name, kwargs))
         return self._env_factory(kwargs=kwargs)
 
     def generate_random_real_env(self):
         # generate a real environment with random parameters within specified range
         kwargs = self._get_random_parameters()
-        print('Generating random real environment "{}" with parameters {}'.
-              format(self.env_name, kwargs))
+        print('Generating random real environment "{}" with parameters {}'.format(self.env_name, kwargs))
         return self._env_factory(kwargs=kwargs)
 
     def generate_default_virtual_env(self, seed):
         # generate a virtual environment with default parameters
         kwargs = self._get_default_parameters()
-        print('Generating default virtual environment "{}" with parameters {}'.
-              format(self.env_name, kwargs))
-        env = VirtualEnv(state_dim=self.state_dim,
-                         action_dim=self.action_dim,
-                         input_seed=seed,
-                         **kwargs)
+        print('Generating default virtual environment "{}" with parameters {}'.format(self.env_name, kwargs))
+        env = VirtualEnv(state_dim=self.state_dim, action_dim=self.action_dim, input_seed=seed, **kwargs)
         return EnvWrapper(env=env, is_virtual_env=True)
 
     def _env_factory(self, kwargs):
-        if self.env_name == 'PendulumEnv':
+        if self.env_name == "PendulumEnv":
             env = PendulumEnv(**kwargs)
         else:
-            raise NotImplementedError('Environment not supported')
+            raise NotImplementedError("Environment not  supported")
         env.seed(self.seed)
 
         return EnvWrapper(env=env, is_virtual_env=False)
@@ -101,8 +92,7 @@ class EnvFactory:
         kwargs = {}
         for key, value in self.env_config.items():
             if isinstance(value, list):
-                kwargs[key] = np.random.uniform(low=float(value[0]),
-                                                high=float(value[2]))
+                kwargs[key] = np.random.uniform(low=float(value[0]), high=float(value[2]))
             else:
                 kwargs[key] = value
         return kwargs
@@ -120,7 +110,7 @@ class EnvFactory:
 if __name__ == "__main__":
     import yaml
 
-    with open("../default_config.yaml", 'r') as stream:
+    with open("../default_config.yaml", "r") as stream:
         config = yaml.safe_load(stream)
         gen = EnvFactory(config)
         a = gen.generate_default_real_env()
