5bbc1a84700163461163c6f06a89dc8db138fd87
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2020-06-11 23:39:43 +0200

    weight normalization added

--- envs/virtual_env.py
+++ envs/virtual_env.py
@@ -1,7 +1,7 @@
 import gym
-import numpy as np
 import torch
 import torch.nn as nn
+from torch.nn.utils.weight_norm import weight_norm
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -16,15 +16,28 @@ class VirtualEnv(nn.Module, gym.Env):
         self._max_episode_steps = int(kwargs["max_steps"])
 
         hidden_size = kwargs["hidden_size"]
-        self.base = nn.Sequential(
-            nn.Linear(state_dim + action_dim + 1, hidden_size),  # +1 because of seed
-            nn.Tanh(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.Tanh(),
-        )
-        self.state_head = nn.Linear(hidden_size, state_dim)
-        self.reward_head = nn.Linear(hidden_size, 1)
-        self.done_head = nn.Linear(hidden_size, 1)
+        apply_weight_norm = kwargs["weight_norm"]
+
+        if apply_weight_norm:
+            self.base = nn.Sequential(
+                weight_norm(nn.Linear(state_dim + action_dim + 1, hidden_size)),  # +1 because of seed
+                nn.Tanh(),
+                weight_norm(nn.Linear(hidden_size, hidden_size)),
+                nn.Tanh(),
+            )
+            self.state_head = weight_norm(nn.Linear(hidden_size, state_dim))
+            self.reward_head = weight_norm(nn.Linear(hidden_size, 1))
+            self.done_head = weight_norm(nn.Linear(hidden_size, 1))
+        else:
+            self.base = nn.Sequential(
+                nn.Linear(state_dim + action_dim + 1, hidden_size),  # +1 because of seed
+                nn.Tanh(),
+                nn.Linear(hidden_size, hidden_size),
+                nn.Tanh(),
+            )
+            self.state_head = nn.Linear(hidden_size, state_dim)
+            self.reward_head = nn.Linear(hidden_size, 1)
+            self.done_head = nn.Linear(hidden_size, 1)
 
     def reset(self):
         self.state = torch.zeros(self.state_dim, device="cpu")  # not sure what the default state should be
--- models/actor_critic.py
+++ models/actor_critic.py
@@ -1,35 +1,51 @@
 import torch
 import torch.nn as nn
 from torch.distributions.normal import Normal
+from torch.nn.utils.weight_norm import weight_norm
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def build_nn_from_config(input_dim, output_dim, agent_name, config):
-    hidden_size = config['agents'][agent_name]['hidden_size']
-    activation_fn = config['agents'][agent_name]['activation_fn']
+    hidden_size = config["agents"][agent_name]["hidden_size"]
+    activation_fn = config["agents"][agent_name]["activation_fn"]
 
-    if activation_fn == 'relu':
+    apply_weight_norm = config["agents"][agent_name]["weight_norm"]
+
+    if activation_fn == "relu":
         act_fn = nn.ReLU()
-    elif activation_fn == 'tanh':
+    elif activation_fn == "tanh":
         act_fn = nn.Tanh()
 
-    return nn.Sequential(nn.Linear(input_dim, hidden_size), act_fn,
-                         nn.Linear(hidden_size, hidden_size), act_fn,
-                         nn.Linear(hidden_size, output_dim))
+    if apply_weight_norm:
+        return nn.Sequential(
+            weight_norm(nn.Linear(input_dim, hidden_size)),
+            act_fn,
+            weight_norm(nn.Linear(hidden_size, hidden_size)),
+            act_fn,
+            weight_norm(nn.Linear(hidden_size, output_dim)),
+        )
+    else:
+        return nn.Sequential(
+            nn.Linear(input_dim, hidden_size),
+            act_fn,
+            nn.Linear(hidden_size, hidden_size),
+            act_fn,
+            nn.Linear(hidden_size, output_dim),
+        )
 
 
 class Actor(nn.Module):
     def __init__(self, state_dim, action_dim, agent_name, config):
         super(Actor, self).__init__()
 
-        self.net = build_nn_from_config(input_dim=state_dim,
-                                        output_dim=action_dim,
-                                        agent_name=agent_name,
-                                        config=config)
+        self.net = build_nn_from_config(
+            input_dim=state_dim, output_dim=action_dim, agent_name=agent_name, config=config
+        )
+
         self.action_std = torch.nn.Parameter(
-            torch.ones(action_dim, device=device) *
-            config['agents'][agent_name]['action_std'])
+            torch.ones(action_dim, device=device) * config["agents"][agent_name]["action_std"]
+        )
 
     def forward(self, state):
         action_mean = self.net(state)
@@ -48,10 +64,9 @@ class Critic_Q(nn.Module):
     def __init__(self, state_dim, action_dim, agent_name, config):
         super(Critic_Q, self).__init__()
 
-        self.net = build_nn_from_config(input_dim=state_dim + action_dim,
-                                        output_dim=action_dim,
-                                        agent_name=agent_name,
-                                        config=config)
+        self.net = build_nn_from_config(
+            input_dim=state_dim + action_dim, output_dim=action_dim, agent_name=agent_name, config=config
+        )
 
     def forward(self, state, action):
         return self.net(torch.cat([state, action], 1))
@@ -61,10 +76,7 @@ class Critic_V(nn.Module):
     def __init__(self, state_dim, agent_name, config):
         super(Critic_V, self).__init__()
 
-        self.net = build_nn_from_config(input_dim=state_dim,
-                                        output_dim=1,
-                                        agent_name=agent_name,
-                                        config=config)
+        self.net = build_nn_from_config(input_dim=state_dim, output_dim=1, agent_name=agent_name, config=config)
 
     def forward(self, state):
         return self.net(state)
