c8871afc06bc028d5dfd1f702af4d979eafffa18
Thomas <thomasnierhoff@web.de>
2020-07-14 13:56:11 +0200

    added GTN analyze script

--- experiments/GTN_params_reduced_analyze.py
+++ experiments/GTN_params_reduced_analyze.py
@@ -1,19 +1,17 @@
 import ast
+import random
+import colorsys
 import math
-
-import numpy as np
 import hpbandster.core.result as hpres
-import hpbandster.visualization as hpvis
-
-from decimal import Decimal
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
 
 # smallest value is best -> reverse_loss = True
 # largest value is best -> reverse_loss = False
 REVERSE_LOSS = True
 EXP_LOSS = 2
-OUTLIER_PERC = 0.2
+OUTLIER_PERC = 0.0
 
 
 def analyze_bohb(log_dir):
@@ -21,8 +19,43 @@ def analyze_bohb(log_dir):
     result = hpres.logged_results_to_HBS_result(log_dir)
 
     result = remove_outliers(result)
+    result_w = deepcopy(result)
+    result_wo = deepcopy(result)
 
     analyze_result_order(result)
+
+    result_w = get_w(result_w, inverse=False)
+    result_wo = get_w(result_wo, inverse=True)
+
+    print(len(result_w.data))
+    print(len(result_wo.data))
+
+    plot_accuracy_over_budget(result_w)
+    plot_accuracy_over_budget(result_wo)
+
+    plt.show()
+
+
+def get_w(result, inverse=False):
+    del_list = []
+    for key1, value1 in result.data.items():
+        for key2, value2 in value1.results.items():
+            order = ast.literal_eval(value2['info']['order'])
+            if inverse:
+                if 3 not in order:
+                    del_list.append((key1, key2))
+            else:
+                if 3 in order:
+                    del_list.append((key1, key2))
+
+    for key in del_list:
+        key1 = key[0]
+        key2 = key[1]
+        result.data[key1].results.pop(key2, None)
+        if len(result.data[key1].results) == 0:
+            result.data.pop(key1)
+
+    return result
 
 
 def analyze_result_order(result):
@@ -34,7 +67,6 @@ def analyze_result_order(result):
             loss = value2['loss']
             order = ast.literal_eval(value2['info']['order'])
             order_list.append((loss, order))
-
             if 3 in order:
                 w_3.append(loss)
             else:
@@ -46,6 +78,40 @@ def analyze_result_order(result):
 
     print(sum(w_3)/len(w_3))
     print(sum(wo_3)/len(wo_3))
+
+
+
+def plot_accuracy_over_budget(result):
+    fig, ax = plt.subplots()
+
+    # plot hyperband plot
+    index = None
+    color = None
+
+    for key, value1 in result.data.items():
+        if key[0] is not index:
+            index = key[0]
+            color = get_bright_random_color()
+
+        try:
+            x = []
+            y = []
+            for key2, value2 in value1.results.items():
+                x.append(key2)
+                y.append(value2["loss"])
+            plt.semilogx(x, y, color=color, marker=".")
+        except:
+            print('Error in plot_accuracy_over_budget, continuing')
+
+    ax.set_title('Score for different configurations')
+    ax.set_xlabel('epochs')
+    ax.set_ylabel('score')
+
+
+def get_bright_random_color():
+    h, s, l = random.random(), 1, 0.5
+    return colorsys.hls_to_rgb(h, l, s)
+
 
 
 def remove_outliers(result):
