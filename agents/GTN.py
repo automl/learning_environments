c5fe60422a99bb2fb49f7f309ee9bd3f751a6f9f
Thomas <thomasnierhoff@web.de>
2020-09-30 00:12:05 +0200

    log time 2

--- agents/GTN.py
+++ agents/GTN.py
@@ -346,11 +346,12 @@ class GTN_Worker(GTN_Base):
     def run(self):
         # read data from master
         while not self.quit_flag:
-            print('-- Worker {}: read worker inputs'.format(self.id))
+            t1 = time.time()
+            print('-- Worker {}: read worker inputs {}'.format(self.id, time.time()-t1))
 
             self.read_worker_input()
 
-            print('-- Worker {}: evaluation'.format(self.id))
+            print('-- Worker {}: evaluation {}'.format(self.id, time.time()-t1))
 
             time_start = time.time()
 
@@ -377,7 +378,7 @@ class GTN_Worker(GTN_Base):
 
             self.get_random_eps()
 
-            print('-- Worker {}: train add'.format(self.id))
+            print('-- Worker {}: train add {}'.format(self.id, time.time()-t1))
 
             # first mirrored noise +N
             self.add_noise_to_virtual_env()
@@ -394,7 +395,7 @@ class GTN_Worker(GTN_Base):
                                                              time_remaining=self.timeout-(time.time()-time_start),
                                                              gtn_iteration=self.gtn_iteration))
 
-            print('-- Worker {}: train sub'.format(self.id))
+            print('-- Worker {}: train sub {}'.format(self.id, time.time()-t1))
 
             # # second mirrored noise -N
             self.subtract_noise_from_virtual_env()
@@ -411,7 +412,7 @@ class GTN_Worker(GTN_Base):
                                                              time_remaining=self.timeout-(time.time()-time_start),
                                                              gtn_iteration=self.gtn_iteration))
 
-            print('-- Worker {}: calc score'.format(self.id))
+            print('-- Worker {}: calc score {}'.format(self.id, time.time()-t1))
 
             if self.minimize_score:
                 #print('worker ' + str(self.id) + ' ' + str(score_sub) + ' ' + str(score_add) + ' ' + str(weight_sub) + ' ' + str(weight_add))
@@ -446,7 +447,7 @@ class GTN_Worker(GTN_Base):
             # print('-- LOSS ADD: ' + str(score_add))
             # print('-- LOSS SUB: ' + str(score_sub))
             # print('-- LOSS BEST: ' + str(best_score))
-            print('-- Worker {}: write result'.format(self.id))
+            print('-- Worker {}: write result '.format(self.id, time.time()-t1))
 
             self.write_worker_result(score=best_score,
                                      score_orig=score_orig,
@@ -454,7 +455,7 @@ class GTN_Worker(GTN_Base):
                                      time_test=time_test,
                                      time_elapsed = time.time()-time_start)
 
-        print('Agent ' + str(self.id) + ' quitting')
+        print('Worker ' + str(self.id) + ' quitting')
 
     def read_worker_input(self):
         file_name = self.get_input_file_name(id=self.id)
