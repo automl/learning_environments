c69e127fb7999194700ae1e2dc0728a587acbf64
Thomas <thomasnierhoff@web.de>
2020-10-05 11:07:12 +0200

    added parameterization for initial gridworld experiment

--- experiments/GTNC_params_bohb_gridworld_initial.py
+++ experiments/GTNC_params_bohb_gridworld_initial.py
@@ -26,14 +26,14 @@ class ExperimentWrapper():
         cs = CS.ConfigurationSpace()
 
         cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='gtn_noise_std', lower=1e-2, upper=1, log=True, default_value=1e-1))
-        cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='gtn_step_size', lower=1e-2, upper=5, log=True, default_value=1))
+        cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='gtn_step_size', lower=1e-1, upper=10, log=True, default_value=1))
         cs.add_hyperparameter(CSH.CategoricalHyperparameter(name='gtn_num_grad_evals', choices=[1,2,3], default_value=1))
         cs.add_hyperparameter(CSH.CategoricalHyperparameter(name='gtn_grad_eval_type', choices=['mean', 'minmax'], default_value='minmax'))
-        cs.add_hyperparameter(CSH.UniformIntegerHyperparameter(name='gtn_score_transform_type', lower=0, upper=6, log=False, default_value=5))
-        cs.add_hyperparameter(CSH.CategoricalHyperparameter(name='gtn_exploration_gain', choices=[0, 1e-4], default_value=0))
-        cs.add_hyperparameter(CSH.CategoricalHyperparameter(name='gtn_correct_path_gain', choices=[0, 1e-4], default_value=0))
+        cs.add_hyperparameter(CSH.UniformIntegerHyperparameter(name='gtn_score_transform_type', lower=1, upper=6, log=False, default_value=5))
+        cs.add_hyperparameter(CSH.CategoricalHyperparameter(name='gtn_exploration_gain', choices=[0, 0.001], default_value=0))
+        cs.add_hyperparameter(CSH.CategoricalHyperparameter(name='gtn_correct_path_gain', choices=[0, 0.001], default_value=0))
 
-        cs.add_hyperparameter(CSH.UniformIntegerHyperparameter(name='ql_init_episodes', lower=2, upper=10, log=False, default_value=5))
+        cs.add_hyperparameter(CSH.UniformIntegerHyperparameter(name='ql_init_episodes', lower=0, upper=10, log=False, default_value=5))
         cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='ql_alpha', lower=0.01, upper=1, log=True, default_value=0.5))
         cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='ql_gamma', lower=0.5, upper=1, log=False, default_value=1))
         cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='ql_eps_init', lower=0.01, upper=1, log=True, default_value=1))
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     # torch.cuda.manual_seed_all(SEED)
 
     x = datetime.datetime.now()
-    run_id = 'GTNC_params_bohb_' + x.strftime("%Y-%m-%d-%H")
+    run_id = 'GTNC_params_bohb_gridworld_initial_' + x.strftime("%Y-%m-%d-%H")
 
     if len(sys.argv) > 1:
         for arg in sys.argv[1:]:
