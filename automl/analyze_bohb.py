ddf6a8c585ee4a91d46db6aafff93e7968b419ab
Thomas <thomasnierhoff@web.de>
2020-10-08 11:56:54 +0200

    reduce Pendulum layer number

--- automl/analyze_bohb.py
+++ automl/analyze_bohb.py
@@ -393,7 +393,8 @@ def get_bright_random_color():
 if __name__ == '__main__':
     #log_dir = '../results/TD3_params_bohb_2020-07-07-12'
     #log_dir = '../results/GTN_params_reduced_bohb_2020-07-18-06-pen-latest-greatest2'
-    log_dir = '../results/TD3_params_bohb_2020-07-07-12'
+    #log_dir = '../results/TD3_params_bohb_2020-07-07-12'
+    log_dir = '../results'
     analyze_bohb(log_dir)
 
 
