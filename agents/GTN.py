8dc2a762ac016825212372445dd8524184e52689
Thomas <thomasnierhoff@web.de>
2020-11-06 17:41:58 +0100

    change working dir

--- agents/GTN.py
+++ agents/GTN.py
@@ -38,8 +38,8 @@ class GTN_Base(nn.Module):
 
         self.env_factory = EnvFactory(config)
         self.virtual_env_orig = self.env_factory.generate_virtual_env(print_str='GTN_Base: ')
-        self.model_dir = str(os.path.join(os.getcwd(), "results", 'GTN_models__cartpole'))
-        self.working_dir = str(os.path.join(os.getcwd(), "results", 'GTN_sync__cartpole'))
+        self.model_dir = str(os.path.join(os.getcwd(), "results", 'GTN_models_gridworld'))
+        self.working_dir = str(os.path.join(os.getcwd(), "results", 'GTN_sync_gridworld'))
 
         os.makedirs(self.working_dir, exist_ok=True)
 
