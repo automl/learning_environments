4f2f8418dc68a2dc1923d3a5c1781fd37b1e36c6
Thomas <thomasnierhoff@web.de>
2020-11-20 11:24:59 +0100

    added vary hp experiment results + script

---
+++ experiments/GTNC_visualize_cartpole_vary_hp.py
@@ -0,0 +1,42 @@
+import os
+import torch
+import statistics
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+FILE_DIR = '/home/dingsda/master_thesis/learning_environments/results/GTNC_evaluate_cartpole_vary_hp_2020-11-17-10/GTN_models_CartPole-v0'
+FILE_LIST = ['0.pt', '2.pt', '1.pt']
+
+
+if __name__ == "__main__":
+    data_list = []
+    mean_list = []
+    for file in FILE_LIST:
+        file_path = os.path.join(FILE_DIR, file)
+        save_dict = torch.load(file_path)
+        reward_list = save_dict['reward_list']
+
+        reward_list_single = []
+        for r_list in reward_list:
+            reward_list_single += r_list
+
+        data_list.append(reward_list_single)
+        mean_list.append('mean: ' + str(statistics.mean(reward_list_single)))
+
+    data_dict = {'train: real  / HP vary': data_list[0],
+                 'train: synth. / HP vary': data_list[1],
+                 'train: synth. / no HP vary': data_list[2]}
+
+    df = pd.DataFrame(data=data_dict)
+    plt.figure(dpi=600, figsize=(6.5,3))
+    ax = sns.violinplot(data=df, cut=0, inner=None)
+    plt.ylabel('cumulative reward')
+
+    for x,y,mean in zip([0,1,2], [220,220,220], mean_list):
+        plt.text(x,y,mean, ha='center', va='center')
+
+    plt.savefig('cartpole_vary_hp.svg', bbox_inches='tight')
+    plt.show()
+
+
