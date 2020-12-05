86b0ed7456de0236e2e674b5c8987d3e67016a4e
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2020-12-05 14:38:45 +0100

    implemented Dueling DDQN for DDQN->DuelingDDQN transfer

--- experiments/bohb_params_DuelingDDQN.py
+++ experiments/bohb_params_DuelingDDQN.py
@@ -1,16 +1,18 @@
 import datetime
-import sys
-import yaml
 import random
+import sys
 import time
-import numpy as np
-import torch
+from copy import deepcopy
+
 import ConfigSpace as CS
 import ConfigSpace.hyperparameters as CSH
-from copy import deepcopy
+import numpy as np
+import torch
+import yaml
+
 from agents.DuelingDDQN import DuelingDDQN
-from envs.env_factory import EnvFactory
 from automl.bohb_optim import run_bohb_parallel, run_bohb_serial
+from envs.env_factory import EnvFactory
 
 
 class ExperimentWrapper():
@@ -24,7 +26,6 @@ class ExperimentWrapper():
 
         return params
 
-
     def get_configspace(self):
         cs = CS.ConfigurationSpace()
 
@@ -33,37 +34,35 @@ class ExperimentWrapper():
         cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='gamma', lower=0.001, upper=0.1, log=True, default_value=0.01))
         cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='lr', lower=1e-4, upper=1e-1, log=True, default_value=3e-4))
         cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='tau', lower=0.001, upper=0.1, log=True, default_value=0.01))
-        cs.add_hyperparameter(CSH.UniformIntegerHyperparameter(name='policy_delay', lower=1, upper=5, log=False, default_value=2))
+        cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='eps_init', lower=0.8, upper=1, log=True, default_value=0.9))
+        cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='eps_min', lower=0.005, upper=0.05, log=True, default_value=0.05))
+        cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='eps_decay', lower=0.01, upper=0.2, log=True, default_value=0.1))
         cs.add_hyperparameter(CSH.UniformIntegerHyperparameter(name='rb_size', lower=1000, upper=1000000, log=True, default_value=100000))
         cs.add_hyperparameter(CSH.UniformIntegerHyperparameter(name='hidden_size', lower=64, upper=512, log=True, default_value=128))
         cs.add_hyperparameter(CSH.UniformIntegerHyperparameter(name='feature_dim', lower=32, upper=512, log=True, default_value=128))
         cs.add_hyperparameter(CSH.CategoricalHyperparameter(name='activation_fn', choices=['relu', 'tanh', 'leakyrelu', 'prelu'], default_value='relu'))
-        cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='action_std', lower=0.01, upper=10, log=True, default_value=0.1))
-        cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='policy_std', lower=0.01, upper=10, log=True, default_value=0.1))
         cs.add_hyperparameter(CSH.UniformIntegerHyperparameter(name='early_out_num', lower=1, upper=5, log=True, default_value=3))
 
         return cs
-
 
     def get_specific_config(self, cso, default_config, budget):
         config = deepcopy(default_config)
 
         config["agents"]["duelingddqn"]["init_episodes"] = cso["init_episodes"]
         config["agents"]["duelingddqn"]["batch_size"] = cso["batch_size"]
-        config["agents"]["duelingddqn"]["gamma"] = 1-cso["gamma"]
+        config["agents"]["duelingddqn"]["gamma"] = 1 - cso["gamma"]
         config["agents"]["duelingddqn"]["lr"] = cso["lr"]
         config["agents"]["duelingddqn"]["tau"] = cso["tau"]
-        config["agents"]["duelingddqn"]["policy_delay"] = cso["policy_delay"]
+        config["agents"]["duelingddqn"]["eps_init"] = cso["eps_init"]
+        config["agents"]["duelingddqn"]["eps_min"] = cso["eps_min"]
+        config["agents"]['duelingddqn']['eps_decay'] = 1 - cso["eps_decay"]
         config["agents"]["duelingddqn"]["rb_size"] = cso["rb_size"]
         config["agents"]["duelingddqn"]["hidden_size"] = cso["hidden_size"]
         config["agents"]["duelingddqn"]["feature_dim"] = cso["feature_dim"]
         config["agents"]["duelingddqn"]["activation_fn"] = cso["activation_fn"]
-        config["agents"]["duelingddqn"]["action_std"] = cso["action_std"]
-        config["agents"]["duelingddqn"]["policy_std"] = cso["policy_std"]
         config["agents"]["duelingddqn"]["early_out_num"] = cso["early_out_num"]
 
         return config
-
 
     def compute(self, working_dir, bohb_id, config_id, cso, budget, *args, **kwargs):
         with open("default_config_cartpole.yaml", 'r') as stream:
@@ -84,8 +83,7 @@ class ExperimentWrapper():
         env = env_fac.generate_real_env()
 
         dueling_ddqn = DuelingDDQN(env=env,
-                  max_action=env.get_max_action(),
-                  config=config)
+                                   config=config)
         rewards, _ = dueling_ddqn.train(env)
         score = len(rewards)
 
@@ -97,9 +95,9 @@ class ExperimentWrapper():
         print('----------------------------')
 
         return {
-            "loss": score,
-            "info": info
-        }
+                "loss": score,
+                "info": info
+                }
 
 
 if __name__ == "__main__":
@@ -109,10 +107,10 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         for arg in sys.argv[1:]:
             print(arg)
-        random.seed(int(sys.argv[1])+int(time.time()))
-        np.random.seed(int(sys.argv[1])+int(time.time()))
-        torch.manual_seed(int(sys.argv[1])+int(time.time()))
-        torch.cuda.manual_seed_all(int(sys.argv[1])+int(time.time()))
+        random.seed(int(sys.argv[1]) + int(time.time()))
+        np.random.seed(int(sys.argv[1]) + int(time.time()))
+        torch.manual_seed(int(sys.argv[1]) + int(time.time()))
+        torch.cuda.manual_seed_all(int(sys.argv[1]) + int(time.time()))
         res = run_bohb_parallel(id=sys.argv[1],
                                 bohb_workers=sys.argv[2],
                                 run_id=run_id,
