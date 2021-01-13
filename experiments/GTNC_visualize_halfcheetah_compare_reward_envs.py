b68002441eba6d80445dd7df897e586af279f8a0
Thomas <thomasnierhoff@web.de>
2021-01-13 19:45:30 +0100

    adapted parameters

--- experiments/GTNC_visualize_halfcheetah_compare_reward_envs.py
+++ experiments/GTNC_visualize_halfcheetah_compare_reward_envs.py
@@ -1,138 +1,64 @@
-import os
-import sys
-
+import matplotlib.pyplot as plt
 import torch
-import hpbandster.core.result as hpres
-import hpbandster.visualization as hpvis
+import numpy as np
 
-from agents.agent_utils import select_agent
-from envs.env_factory import EnvFactory
-from models.baselines import ICMTD3
+LOG_FILES = ['../results/halfcheetah_compare_reward_envs/best5_0.pt',
+             '../results/halfcheetah_compare_reward_envs/best5_2.pt',
+             '../results/halfcheetah_compare_reward_envs/best5_4.pt',
+             '../results/halfcheetah_compare_reward_envs/best5_102.pt']
 
-SAVE_DIR = '/home/nierhoff/master_thesis/learning_environments/results/halfcheetah_compare_reward_envs'
+STD_MULT = 0.5
+MAX_VALS = 15
 
-LOG_DICT = {}
-LOG_DICT['2'] = '/home/nierhoff/master_thesis/learning_environments/results/GTNC_evaluate_halfcheetah_2021-01-11-19_2'
-LOG_DICT['4'] = '/home/nierhoff/master_thesis/learning_environments/results/GTNC_evaluate_halfcheetah_2021-01-11-19_4'
-LOG_DICT['102']= '/home/nierhoff/master_thesis/learning_environments/results/GTNC_evaluate_halfcheetah_2021-01-12-11_102'
+def get_data():
+    list_data = []
+    for log_file in LOG_FILES:
+        data = torch.load(log_file)
+        list_data.append(data['reward_list'])
 
-MODEL_NUM = 5
-MODEL_AGENTS = 3
+    # copy from list to numpy array
+    proc_data = []
 
-def get_best_models_from_log(log_dir):
-    result = hpres.logged_results_to_HBS_result(log_dir)
-    all_runs = result.get_all_runs()
-    id2conf = result.get_id2config_mapping()
+    n = len(list_data[0][0])
+    for data in list_data:
+        np_data = np.zeros([MAX_VALS,n])
 
-    best_models = []
+        for i in range(len(np_data)):
+            np_data[i] = np.array(data[i])
 
-    for value in result.data.values():
-        try:
-            loss = value.results[1.0]['loss']
-            model_name = value.results[1.0]['info']['model_name']
+        mean = np.mean(np_data,axis=0)
+        std = np.std(np_data,axis=0)
 
-            if not os.path.isfile(model_name):
-                model_name = model_name.replace('nierhoff', 'dingsda')
-            best_models.append((loss, model_name))
-        except:
-            continue
+        proc_data.append((mean,std))
 
-    best_models.sort(key=lambda x: x[0])
-    best_models = best_models[:MODEL_NUM]
-
-    return best_models
+    return proc_data
 
 
-def load_envs_and_config(model_file):
-    file_path = os.path.join(model_file)
-    save_dict = torch.load(file_path)
+def plot_data(proc_data, savefig_name):
+    fig, ax = plt.subplots(dpi=600, figsize=(5,4))
+    colors = []
+    #
+    # for mean, _ in data_w:
+    #     plt.plot(mean_w)
+    #     colors.append(plt.gca().lines[-1].get_color())
 
-    config = save_dict['config']
-    config['device'] = 'cuda'
-    config['envs']['HalfCheetah-v3']['solved_reward'] = 100000  # something big enough to prevent early out triggering
+    for mean, std in proc_data:
+        plt.plot(mean)
 
-    env_factory = EnvFactory(config=config)
-    reward_env = env_factory.generate_reward_env()
-    reward_env.load_state_dict(save_dict['model'])
-    real_env = env_factory.generate_real_env()
+    for mean, std in proc_data:
+        plt.fill_between(x=range(len(mean)), y1=mean - std * STD_MULT, y2=mean + std * STD_MULT, alpha=0.1)
 
-    return reward_env, real_env, config
-
-
-def train_test_agents(mode, env, real_env, config):
-    rewards = []
-
-    # settings for comparability
-    config['agents']['td3']['test_episodes'] = 1
-    config['agents']['td3']['train_episodes'] = 100
-    config['agents']['td3']['print_rate'] = 1
-
-    for i in range(MODEL_AGENTS):
-        if mode == '-1':
-            agent = ICMTD3(env=real_env, max_action=real_env.get_max_action(), config=config)
-        else:
-            agent = select_agent(config=config, agent_name='td3')
-        reward, _ = agent.train(env=env, real_env=real_env, test_env=real_env)
-        print('reward: ' + str(reward))
-        rewards.append(reward)
-
-    return rewards
-
-
-def save_list(mode, config, reward_list):
-
-    os.makedirs(SAVE_DIR, exist_ok=True)
-    file_name = os.path.join(SAVE_DIR, 'best' + str(MODEL_NUM) + '_' + str(mode) + '.pt')
-    save_dict = {}
-    save_dict['config'] = config
-    save_dict['reward_list'] = reward_list
-    print(reward_list)
-    torch.save(save_dict, file_name)
-
-
-def eval_models(mode, log_dir):
-    best_models = get_best_models_from_log(log_dir)
-
-    reward_list = []
-    for _, model_file in best_models:
-        reward_env, real_env, config = load_envs_and_config(model_file)
-        rewards = train_test_agents(mode=mode, env=reward_env, real_env=real_env, config=config)
-        reward_list += rewards
-    save_list(mode, config, reward_list)
-
-
-def eval_base(mode, log_dir):
-    best_models = get_best_models_from_log(log_dir)
-
-    reward_list = []
-    for i in range(MODEL_NUM):
-        reward_env, real_env, config = load_envs_and_config(best_models[0][1])
-        rewards = train_test_agents(mode=mode, env=real_env, real_env=real_env, config=config)
-        reward_list += rewards
-    save_list(mode, config, reward_list)
-
-
-def eval_icm(mode, log_dir):
-    best_models = get_best_models_from_log(log_dir)
-
-    reward_list = []
-    for i in range(MODEL_NUM):
-        reward_env, real_env, config = load_envs_and_config(best_models[0][1])
-        rewards = train_test_agents(mode=mode, env=real_env, real_env=real_env, config=config)
-        reward_list += rewards
-    save_list(mode, config, reward_list)
-
+    plt.legend(('baseline naive','NN w/o info vector','NN w/ info vector','baseline Kaust'))
+    plt.xlim(0,99)
+    plt.title('HalfCheetah-v3')
+    plt.xlabel('episode')
+    plt.ylabel('average reward')
+    plt.savefig(savefig_name)
+    plt.show()
 
 if __name__ == "__main__":
+    proc_data = get_data()
+    plot_data(proc_data=proc_data, savefig_name='halfcheetah_compare_reward_env.png')
 
-    for arg in sys.argv[1:]:
-        print(arg)
-    mode = str(sys.argv[1])
 
-    if mode == '-1':
-        eval_icm(mode=mode, log_dir=LOG_DICT['2'])
-    elif mode == '0':
-        eval_base(mode=mode, log_dir=LOG_DICT['2'])
-    else:
-        eval_models(mode=mode, log_dir=LOG_DICT[mode])
 
