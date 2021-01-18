6457c3f030f2098747609e850e50808bf0d5a416
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2021-01-18 18:10:05 +0100

    gave ICM a separate optimizer, made its networks larger and implemented ICM for DDQN

--- models/baselines.py
+++ models/baselines.py
@@ -1,11 +1,15 @@
-import torch.nn as nn
-from models.model_utils import build_nn_from_config
-from agents.TD3 import TD3
-import yaml
 import time
+
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
+import yaml
+
+from agents.DDQN import DDQN
+from agents.TD3 import TD3
 from envs.env_factory import EnvFactory
+from models.model_utils import build_nn_from_config
+from utils import to_one_hot_encoding
 
 
 class ICMModel(nn.Module):
@@ -16,13 +20,29 @@ class ICMModel(nn.Module):
         self.action_dim = action_dim
         self.feature_dim = feature_dim
 
-        nn_features_config = {'hidden_size': 128, 'hidden_layer': 2, 'activation_fn': 'leakyrelu'}
-        nn_inverse_config = {'hidden_size': 128, 'hidden_layer': 1, 'activation_fn': 'relu'}
-        nn_forward_pre_config = {'hidden_size': 128, 'hidden_layer': 1, 'activation_fn': 'leakyrelu'}
-        nn_forward_post_config = {'hidden_size': 128, 'hidden_layer': 1, 'activation_fn': 'identity'}
+        nn_features_config = {
+                'hidden_size': 256,
+                'hidden_layer': 2,
+                'activation_fn': 'leakyrelu'
+                }
+        nn_inverse_config = {
+                'hidden_size': 256,
+                'hidden_layer': 2,
+                'activation_fn': 'relu'
+                }
+        nn_forward_pre_config = {
+                'hidden_size': 256,
+                'hidden_layer': 2,
+                'activation_fn': 'leakyrelu'
+                }
+        nn_forward_post_config = {
+                'hidden_size': 256,
+                'hidden_layer': 1,
+                'activation_fn': 'identity'
+                }
 
         self.features_model = build_nn_from_config(input_dim=state_dim, output_dim=feature_dim, nn_config=nn_features_config)
-        self.inverse_model = build_nn_from_config(input_dim=feature_dim*2, output_dim=action_dim, nn_config=nn_inverse_config)
+        self.inverse_model = build_nn_from_config(input_dim=feature_dim * 2, output_dim=action_dim, nn_config=nn_inverse_config)
 
         self.forward_pre_model = build_nn_from_config(input_dim=action_dim + feature_dim,
                                                       output_dim=feature_dim,
@@ -33,12 +53,12 @@ class ICMModel(nn.Module):
             def __init__(self, input_dim, output_dim):
                 super().__init__()
                 self.fc1 = nn.Sequential(
-                                nn.Linear(input_dim, output_dim),
-                                nn.LeakyReLU(inplace=True),
-                           )
+                        nn.Linear(input_dim, output_dim),
+                        nn.LeakyReLU(inplace=True),
+                        )
                 self.fc2 = nn.Sequential(
-                                nn.Linear(input_dim, output_dim)
-                           )
+                        nn.Linear(input_dim, output_dim)
+                        )
 
             def forward(self, feature, action):
                 x = feature
@@ -83,19 +103,16 @@ class ICMTD3(TD3):
     """
     TD3 with an Intrinsic Curiosity Module ("Curiosity-driven Exploration by Self-supervised Prediction", Pathak et al.)
     """
+
     def __init__(self, env, max_action, config):
         super(ICMTD3, self).__init__(env=env, max_action=max_action, config=config)
 
-        # from the paper:
-        self.beta = .2
-        self.lambd = .1
-        #self.icm_lr = 1e-3
+        self.beta = .5
+        self.icm_lr = 1e-3
         self.icm = ICMModel(state_dim=self.state_dim, action_dim=self.action_dim, feature_dim=128).to(self.device)
 
-        # overwrite actor optimizer, could also add ICM loss to critic loss
-        params = list(self.icm.parameters()) + list(self.actor.parameters())
-        #self.icm_optimizer = torch.optim.Adam(icm_params, lr=self.lr)
-        self.actor_optimizer = torch.optim.Adam(params, lr=self.lr)
+        icm_params = list(self.icm.parameters())
+        self.icm_optimizer = torch.optim.Adam(icm_params, lr=self.icm_lr)
 
     def learn(self, replay_buffer, env, episode):
         self.total_it += 1
@@ -129,20 +146,23 @@ class ICMTD3(TD3):
         critic_loss.backward()
         self.critic_optimizer.step()
 
+        # ICM part
+        next_states_encoded, next_states_pred_encoded, actions_pred = self.icm(input=(states, next_states, actions))
+
+        # compute ICM loss
+        icm_loss = (1 - self.beta) * F.mse_loss(actions_pred, actions) + \
+                   self.beta * F.mse_loss(next_states_encoded, next_states_pred_encoded)
+
+        # Optimize ICM
+        self.icm_optimizer.zero_grad()
+        icm_loss.backward()
+        self.icm_optimizer.step()
+
         # Delayed policy updates
         if self.total_it % self.policy_delay == 0:
             # Compute actor loss
             # todo: check algorithm 1 in original paper; has additional multiplicative term here
             actor_loss = (-self.critic_1(states, self.actor(states))).mean()
-
-            # ICM part
-            next_states_encoded, next_states_pred_encoded, actions_pred = self.icm(input=(states, next_states, actions))
-
-            # compute ICM loss
-            icm_loss = (1 - self.beta) * F.mse_loss(actions_pred, actions) + \
-                       self.beta * F.mse_loss(next_states_encoded, next_states_pred_encoded)
-
-            actor_loss = self.lambd * actor_loss + icm_loss
 
             # Optimize the actor
             self.actor_optimizer.zero_grad()
@@ -160,17 +180,114 @@ class ICMTD3(TD3):
                 target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
 
 
+class ICMDDQN(DDQN):
+    """
+    DDQN with an Intrinsic Curiosity Module ("Curiosity-driven Exploration by Self-supervised Prediction", Pathak et al.)
+    """
+
+    def __init__(self, env, config):
+        super(ICMDDQN, self).__init__(env=env, config=config)
+
+        self.beta = .5
+        self.icm_lr = 1e-3
+        actual_action_dim = len(env.get_random_action())
+        self.icm = ICMModel(state_dim=self.state_dim, action_dim=actual_action_dim, feature_dim=128).to(self.device)
+        icm_params = list(self.icm.parameters())
+        self.icm_optimizer = torch.optim.Adam(icm_params, lr=self.icm_lr)
+
+    def learn(self, replay_buffer, env, episode):
+        self.it += 1
+
+        states, actions, next_states, rewards, dones = replay_buffer.sample(self.batch_size)
+
+        states = states.squeeze()
+        actions = actions.squeeze()
+        next_states = next_states.squeeze()
+        rewards = rewards.squeeze()
+        dones = dones.squeeze()
+
+        if env.has_discrete_state_space():
+            states = to_one_hot_encoding(states, self.state_dim)
+            next_states = to_one_hot_encoding(next_states, self.state_dim)
+
+        q_values = self.model(states)
+        next_q_values = self.model(next_states)
+        next_q_state_values = self.model_target(next_states)
+
+        q_value = q_values.gather(1, actions.long().unsqueeze(1)).squeeze(1)
+        next_q_value = next_q_state_values.gather(1, next_q_values.max(1)[1].unsqueeze(1)).squeeze(1)
+        expected_q_value = rewards + self.gamma * next_q_value * (1 - dones)
+        loss = F.mse_loss(q_value, expected_q_value.detach())
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        if len(actions.shape) == 1:
+            actions = actions.unsqueeze(dim=1)
+
+        # ICM part
+        next_states_encoded, next_states_pred_encoded, actions_pred = self.icm(input=(states, next_states, actions))
+
+        # compute ICM loss
+        icm_loss = (1 - self.beta) * F.mse_loss(actions_pred, actions) + self.beta * F.mse_loss(next_states_encoded,
+                                                                                                next_states_pred_encoded)
+        # Optimize ICM
+        self.icm_optimizer.zero_grad()
+        icm_loss.backward()
+        self.icm_optimizer.step()
+
+        # target network update
+        for target_param, param in zip(self.model_target.parameters(), self.model.parameters()):
+            target_param.data.copy_(self.tau * param + (1 - self.tau) * target_param)
+        return loss
+
+
 if __name__ == "__main__":
-    with open("../default_config_pendulum.yaml", "r") as stream:
+    # with open("../default_config_pendulum.yaml", "r") as stream:
+    #     config = yaml.safe_load(stream)
+    #
+    # # generate environment
+    # env_fac = EnvFactory(config)
+    # real_env = env_fac.generate_real_env()
+    #
+    # icmtd3 = ICMTD3(env=real_env,
+    #                 max_action=real_env.get_max_action(),
+    #                 config=config)
+    #
+    # td3 = TD3(env=real_env,
+    #           max_action=real_env.get_max_action(),
+    #           config=config)
+    #
+    # print("training ICM TD3")
+    # t1 = time.time()
+    # icmtd3.train(env=real_env, time_remaining=1200)
+    # print(time.time() - t1)
+    #
+    # print("training TD3")
+    # t1 = time.time()
+    # td3.train(env=real_env, time_remaining=1200)
+    # print(time.time() - t1)
+
+    with open("../default_config_cartpole.yaml", "r") as stream:
         config = yaml.safe_load(stream)
 
     # generate environment
     env_fac = EnvFactory(config)
-    real_env= env_fac.generate_real_env()
-    td3 = ICMTD3(env=real_env,
-              max_action=real_env.get_max_action(),
+    real_env = env_fac.generate_real_env()
+
+    icqmddqn = ICMDDQN(env=real_env,
+                    config=config)
+
+    ddqn = DDQN(env=real_env,
               config=config)
+
+    print("training ICM DDQN")
     t1 = time.time()
-    td3.train(env=real_env, time_remaining=1200)
-    print(time.time()-t1)
-    #td3.train(env=virt_env, time_remaining=5)
+    icqmddqn.train(env=real_env, time_remaining=500)
+    print(time.time() - t1)
+
+    print("training DDQN")
+    t1 = time.time()
+    ddqn.train(env=real_env, time_remaining=500)
+    print(time.time() - t1)
