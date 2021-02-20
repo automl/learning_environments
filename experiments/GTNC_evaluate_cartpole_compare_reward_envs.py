bef56969b787edba3fe30b8b3c493ab1b8f94c3e
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2021-02-20 22:37:15 +0100

    correct ICM baseline

--- experiments/GTNC_evaluate_cartpole_compare_reward_envs.py
+++ experiments/GTNC_evaluate_cartpole_compare_reward_envs.py
@@ -8,7 +8,7 @@ import hpbandster.visualization as hpvis
 from agents.agent_utils import select_agent
 from envs.env_factory import EnvFactory
 
-SAVE_DIR = '/home/ferreira/Projects/learning/environments/results/cartpole_compare_reward_envs'
+SAVE_DIR = '/home/ferreira/Projects/learning_environments/results/cartpole_compare_reward_envs'
 
 LOG_DICT = {}
 LOG_DICT['1'] = '/home/nierhoff/master_thesis/learning_environments/results/GTNC_evaluate_cartpole_2021-01-28-18_1'
--- experiments/GTNC_evaluate_cmc_compare_reward_envs.py
+++ experiments/GTNC_evaluate_cmc_compare_reward_envs.py
@@ -7,7 +7,8 @@ import hpbandster.core.result as hpres
 from agents.agent_utils import select_agent
 from envs.env_factory import EnvFactory
 
-SAVE_DIR = '/home/nierhoff/master_thesis/learning_environments/results/cmc_compare_reward_envs'
+# SAVE_DIR = '/home/nierhoff/master_thesis/learning_environments/results/cmc_compare_reward_envs'
+SAVE_DIR = '/home/ferreira/Projects/learning_environments/results/cmc_compare_reward_envs'
 
 LOG_DICT = {}
 LOG_DICT['1'] = '/home/nierhoff/master_thesis/learning_environments/results/GTNC_evaluate_cmc_subopt_2021-01-21-09_1'
