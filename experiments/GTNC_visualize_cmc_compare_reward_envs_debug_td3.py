5637dc08013a3fee10d82f7a7fbb521d2086a176
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2021-02-23 12:56:44 +0100

    adapted result path for running reward shaping compare experiment

---
+++ experiments/GTNC_visualize_cmc_compare_reward_envs_debug_td3.py
@@ -0,0 +1,99 @@
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
+from datetime import datetime
+
+LOG_FILES = [
+             # '../results/debug_cmc_td3/best-1.pt',
+             '../results/debug_cmc_td3/best0.pt'
+             ]
+
+STD_MULT = 0.2
+# STD_MULT = 1.
+MIN_STEPS = 100000
+
+IMPORTANT_KEYS = ["action_std", "activiation_fn", "gamma", "lr", "policy_delay", "policy_std", "policy_std_clip", "tau", "same_action_num"]
+
+def get_data():
+    list_data = []
+    file_name_hyperparameters = ""
+    for log_file in LOG_FILES:
+        data = torch.load(log_file)
+        list_data.append((data['reward_list'], data['episode_length_list']))
+        model_num = data['model_num']
+        model_agents = data['model_agents']
+        for k, v in data["config"]["agents"]["td3"].items():
+            if k in IMPORTANT_KEYS:
+                file_name_hyperparameters += f"_{k}_{v}"
+
+
+    min_steps = float('Inf')
+    # get minimum number of evaluations
+    for reward_list, episode_length_list in list_data:
+        for episode_lengths in episode_length_list:
+            print(sum(episode_lengths))
+            min_steps = min(min_steps, sum(episode_lengths))
+
+    min_steps = max(min_steps, MIN_STEPS)
+    # convert data from episodes to steps
+    proc_data = []
+
+    for reward_list, episode_length_list in list_data:
+        np_data = np.zeros([model_num*model_agents,min_steps])
+
+        for it, data in enumerate(zip(reward_list, episode_length_list)):
+            rewards, episode_lengths = data
+
+            concat_list = []
+            rewards = rewards
+
+            for i in range(len(episode_lengths)):
+                concat_list += [rewards[i]]*episode_lengths[i]
+
+            while len(concat_list) < min_steps:
+                concat_list.append(concat_list[-1])
+
+            np_data[it] = np.array(concat_list[:min_steps])
+
+        mean = np.mean(np_data,axis=0)
+        std = np.std(np_data,axis=0)
+
+        proc_data.append((mean,std))
+
+    return proc_data, file_name_hyperparameters
+
+
+def plot_data(proc_data, savefig_name):
+    fig, ax = plt.subplots(dpi=600, figsize=(5,4))
+    colors = []
+    #
+    # for mean, _ in data_w:
+    #     plt.plot(mean_w)
+    #     colors.append(plt.gca().lines[-1].get_color())
+
+    for mean, std in proc_data:
+        plt.plot(mean)
+
+    for mean, std in proc_data:
+        plt.fill_between(x=range(len(mean)), y1=mean - std * STD_MULT, y2=mean + std * STD_MULT, alpha=0.1)
+
+    plt.legend(('TD3 + exc. pot. RN', 'TD3 + add. pot. RN', 'TD3 + exc. non-pot. RN', 'TD3 + add. non-pot. RN', 'TD3', 'TD3 + ICM'), fontsize=7)
+    #plt.xlim(0,99)
+    plt.subplots_adjust(bottom=0.15, left=0.15)
+    plt.title('MountainCarContinuous-v0')
+    plt.xlabel('steps')
+    plt.xlim(0,100000)
+    # plt.xlim(0, 60000)
+    plt.ylim(-75,100)
+    plt.ylabel('cumulative reward')
+    plt.savefig(savefig_name)
+    plt.show()
+
+if __name__ == "__main__":
+    proc_data, file_name_hyperparameters = get_data()
+    time = datetime.now().strftime("%Y_%m_%d_%I_%M_%S")
+    file_name = "../results/debug_cmc_td3/" + time + "_cmc_compare_reward_env" + file_name_hyperparameters + ".png"
+    plot_data(proc_data=proc_data, savefig_name=file_name)
+
+
+
