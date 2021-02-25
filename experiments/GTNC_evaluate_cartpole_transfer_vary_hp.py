904d7bc867c54b4016f7aa325d9356bb8423b3d4
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2021-02-25 18:01:12 +0100

    set init_episodes explicitly

--- experiments/GTNC_evaluate_cartpole_transfer_vary_hp.py
+++ experiments/GTNC_evaluate_cartpole_transfer_vary_hp.py
@@ -11,7 +11,7 @@ from agents.agent_utils import select_agent
 from envs.env_factory import EnvFactory
 
 # SAVE_DIR = '/home/nierhoff/master_thesis/learning_environments/results/cartpole_compare_reward_envs'
-SAVE_DIR = '/home/ferreira/Projects/learning_environments/results/cartpole_compare_reward_envs'
+SAVE_DIR = '/home/ferreira/Projects/learning_environments/results/cartpole_compare_reward_envs_5000_train_eps'
 
 LOG_DICT = {}
 LOG_DICT['1'] = '/home/nierhoff/master_thesis/learning_environments/results/GTNC_evaluate_cartpole_2021-01-28-18_1'
