937f5d7815711b8c0dbfe40558f6cc9dbe7a9b87
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2021-03-08 20:07:08 +0100

    prepared ICM TD3 HalfCheetah BOHB run

--- experiments/bohb_params_TD3_ICM.py
+++ experiments/bohb_params_TD3_ICM.py
@@ -29,7 +29,7 @@ class ExperimentWrapper():
     def get_configspace(self):
         cs = CS.ConfigurationSpace()
 
-        cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='lr', lower=1e-5, upper=1e-3, log=True, default_value=1e-4))
+        cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='lr', lower=1e-6, upper=1e-3, log=True, default_value=1e-4))
         cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='beta', lower=0.001, upper=1.0, log=True, default_value=0.2))
         cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='eta', lower=0.001, upper=1.0, log=True, default_value=0.5))
         cs.add_hyperparameter(CSH.UniformIntegerHyperparameter(name='feature_dim', lower=16, upper=256, log=True, default_value=64))
@@ -49,7 +49,7 @@ class ExperimentWrapper():
         return config
 
     def compute(self, working_dir, bohb_id, config_id, cso, budget, *args, **kwargs):
-        with open("default_config_cmc.yaml", 'r') as stream:
+        with open("default_config_halfcheetah.yaml", 'r') as stream:
             default_config = yaml.safe_load(stream)
 
         config = self.get_specific_config(cso, default_config, budget)
@@ -65,7 +65,6 @@ class ExperimentWrapper():
         # generate environment
         env_fac = EnvFactory(config)
         env = env_fac.generate_real_env()
-
 
         td3 = TD3(env=env,
                   max_action=env.get_max_action(),
@@ -95,7 +94,7 @@ class ExperimentWrapper():
 
 if __name__ == "__main__":
     x = datetime.datetime.now()
-    run_id = 'bohb_params_td3_icm_cmc_' + x.strftime("%Y-%m-%d-%H")
+    run_id = 'bohb_params_td3_icm_hc_' + x.strftime("%Y-%m-%d-%H")
 
     if len(sys.argv) > 1:
         for arg in sys.argv[1:]:
