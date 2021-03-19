df6c99e146256f672bc54be3f4ef50534bba4780
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2021-03-19 20:17:28 +0100

    added PPO ICM BOHB script

---
+++ experiments/bohb_params_PPO_ICM.py
@@ -0,0 +1,136 @@
+import datetime
+import sys
+import yaml
+import ConfigSpace as CS
+import ConfigSpace.hyperparameters as CSH
+from copy import deepcopy
+from agents.PPO import PPO
+from envs.env_factory import EnvFactory
+from automl.bohb_optim import run_bohb_parallel, run_bohb_serial
+import numpy as np
+
+NUM_EVALS = 3
+
+class ExperimentWrapper():
+    def get_bohb_parameters(self):
+        params = {}
+        params['min_budget'] = 1
+        params['max_budget'] = 2
+        params['eta'] = 2
+        params['iterations'] = 1000
+        params['random_fraction'] = 0.3
+
+        return params
+
+
+    def get_configspace(self):
+        cs = CS.ConfigurationSpace()
+
+        cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='lr', lower=1e-6, upper=1e-3, log=True, default_value=1e-4))
+        cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='beta', lower=0.001, upper=1.0, log=True, default_value=0.2))
+        cs.add_hyperparameter(CSH.UniformFloatHyperparameter(name='eta', lower=0.001, upper=1.0, log=True, default_value=0.5))
+        cs.add_hyperparameter(CSH.UniformIntegerHyperparameter(name='feature_dim', lower=16, upper=256, log=True, default_value=64))
+        cs.add_hyperparameter(CSH.UniformIntegerHyperparameter(name='hidden_size', lower=16, upper=256, log=True, default_value=128))
+
+        return cs
+
+
+    def get_specific_config(self, cso, default_config, budget):
+        config = deepcopy(default_config)
+
+        config['agents']['ppo'] = {}
+        config['agents']['ppo']['test_episodes'] = 1
+
+        config['agents']['ppo']['print_rate'] = 100
+
+        config['agents']['ppo']['init_episodes'] = 0
+        config['agents']['ppo']['update_episodes'] = 1
+        config['agents']['ppo']['ppo_epochs'] = 10
+        config['agents']['ppo']['gamma'] = 0.99
+
+        config['agents']['ppo']['lr'] = 1e-5
+        config['agents']['ppo']['vf_coef'] = 1
+        config['agents']['ppo']['ent_coef'] = 0.001
+
+        config['agents']['ppo']['eps_clip'] = 0.2
+        config['agents']['ppo']['rb_size'] = 1000000
+        config['agents']['ppo']['same_action_num'] = 1
+        config['agents']['ppo']['activation_fn'] = 'tanh'
+        config['agents']['ppo']['hidden_size'] = 128
+        config['agents']['ppo']['hidden_layer'] = 2
+
+        config['agents']['ppo']['action_std'] = 0.1
+
+        config['agents']['ppo']['early_out_num'] = 50
+        config['agents']['ppo']['early_out_virtual_diff'] = 0.02
+
+
+        config["agents"]["icm"]["lr"] = cso["lr"]
+        config["agents"]["icm"]["beta"] = cso["beta"]
+        config["agents"]["icm"]["eta"] = cso["eta"]
+        config["agents"]["icm"]["feature_dim"] = cso["feature_dim"]
+        config["agents"]["icm"]["hidden_size"] = cso["hidden_size"]
+        config["device"] = 'cuda'
+
+        return config
+
+
+    def compute(self, working_dir, bohb_id, config_id, cso, budget, *args, **kwargs):
+        with open("default_config_halfcheetah.yaml", 'r') as stream:
+            default_config = yaml.safe_load(stream)
+
+        config = self.get_specific_config(cso, default_config, budget)
+        print('----------------------------')
+        print("START BOHB ITERATION")
+        print('CONFIG: ' + str(config))
+        print('CSO:    ' + str(cso))
+        print('BUDGET: ' + str(budget))
+        print('----------------------------')
+
+        config["agents"]["ppo"]["train_episodes"] = 3000
+
+        info = {}
+
+        # generate environment
+        env_fac = EnvFactory(config)
+        real_env = env_fac.generate_real_env()
+
+        # score = 0
+        rewards_list = []
+        for i in range(NUM_EVALS):
+            ppo = PPO(env=real_env,
+                      config=config,
+                      icm=True)
+            rewards, _, _ = ppo.train(real_env)
+            rewards_list.append(sum(rewards))
+            # score += len(rewards)
+
+        # score = score/NUM_EVALS
+        score = -np.mean(rewards_list)
+        info['config'] = str(config)
+
+        print('----------------------------')
+        print('FINAL SCORE: ' + str(score))
+        print("END BOHB ITERATION")
+        print('----------------------------')
+
+        return {
+            "loss": score,
+            "info": info
+        }
+
+
+if __name__ == "__main__":
+    x = datetime.datetime.now()
+    run_id = 'bohb_params_ppo_hc_icm_1e-3_ent_coef_' + x.strftime("%Y-%m-%d-%H")
+
+    if len(sys.argv) > 1:
+        for arg in sys.argv[1:]:
+            print(arg)
+        res = run_bohb_parallel(id=sys.argv[1],
+                                bohb_workers=sys.argv[2],
+                                run_id=run_id,
+                                experiment_wrapper=ExperimentWrapper())
+    else:
+        res = run_bohb_serial(run_id=run_id,
+                              experiment_wrapper=ExperimentWrapper())
