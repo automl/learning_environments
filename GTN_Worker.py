072077a2e91a2b46fc1ea68ba0e6cd7077a044ee
andreassaelinger <fr_as1464@login1.nemo.privat>
2021-11-10 13:37:25 +0100

    added basic files to run

---
+++ GTN_Worker.py
@@ -0,0 +1,16 @@
+import yaml
+import torch
+import sys
+from agents.GTN import GTN_Worker
+from automl.bohb_optim import run_bohb_parallel, run_bohb_serial
+
+if __name__ == "__main__":
+    torch.set_num_threads(1)
+
+    for arg in sys.argv[1:]:
+        print(arg)
+
+    bohb_id = int(sys.argv[1])
+    id = int(sys.argv[2])
+    worker = GTN_Worker(bohb_id=bohb_id, id=id)
+    worker.run()
