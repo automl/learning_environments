6e8ab712a987ee571fd9de6efd6390784fd18ba8
mrrobot <andreas.saelinger@live.com>
2021-11-11 14:56:25 +0100

    removed BOHB

--- GTNC_evaluate_cartpole.py
+++ GTNC_evaluate_cartpole.py
@@ -2,15 +2,14 @@ import datetime
 import time
 import traceback
 import yaml
-import ConfigSpace as CS
 import random
 import numpy as np
 import torch
 from copy import deepcopy
 from agents.GTN import GTN_Master
-from automl.bohb_optim import run_bohb_parallel
 from communicate.tcp_master_selector import start_communication_thread
 import argparse
+from automl.bohb_optim import get_working_dir
 
 
 def my_parse():  # --bohb_id AAA --id BBB --moab_id CCC --port DDD --min_workers EEE --number_workers FFF --mode DDD
@@ -33,65 +32,35 @@ def seed_experiment(number):
     torch.cuda.manual_seed_all(number + int(time.time()))
 
 
-class ExperimentWrapper():
+def compute(self, working_dir, bohb_id):
+    with open("default_config_cartpole_syn_env.yaml", 'r') as stream:
+        default_config = yaml.safe_load(stream)
 
-    def get_bohb_parameters(self):
-        params = {}
-        params['min_budget'] = 1
-        params['max_budget'] = 1
-        params['eta'] = 2
-        params['random_fraction'] = 1
-        params['iterations'] = 10000
+    config = deepcopy(default_config)
 
-        return params
+    try:
+        gtn = GTN_Master(config, bohb_id=bohb_id, bohb_working_dir=working_dir)
+        _, score_list = gtn.run()
+        score = len(score_list)
+        error = ""
+    except:
+        score = float('Inf')
+        score_list = []
+        error = traceback.format_exc()
+        print(error)
 
-    def get_configspace(self):
-        cs = CS.ConfigurationSpace()
+    info = {'error': str(error), 'score_list': str(score_list)}
 
-        return cs
+    print('----------------------------')
+    print('FINAL SCORE: ' + str(score))
+    print('SCORE LIST:  ' + str(score_list))
+    print("END BOHB ITERATION")
+    print('----------------------------')
 
-    def get_specific_config(self, cso, default_config, budget):
-        config = deepcopy(default_config)
-        return config
-
-    def compute(self, working_dir, bohb_id, config_id, cso, budget, *args, **kwargs):
-        with open("default_config_cartpole_syn_env.yaml", 'r') as stream:
-            default_config = yaml.safe_load(stream)
-
-        config = self.get_specific_config(cso, default_config, budget)
-
-        print('----------------------------')
-        print("START BOHB ITERATION")
-        print('CONFIG: ' + str(config))
-        print('CSO:    ' + str(cso))
-        print('BUDGET: ' + str(budget))
-        print('----------------------------')
-
-        try:
-            gtn = GTN_Master(config, bohb_id=bohb_id, bohb_working_dir=working_dir)
-            _, score_list = gtn.run()
-            score = len(score_list)
-            error = ""
-        except:
-            score = float('Inf')
-            score_list = []
-            error = traceback.format_exc()
-            print(error)
-
-        info = {}
-        info['error'] = str(error)
-        info['score_list'] = str(score_list)
-
-        print('----------------------------')
-        print('FINAL SCORE: ' + str(score))
-        print('SCORE LIST:  ' + str(score_list))
-        print("END BOHB ITERATION")
-        print('----------------------------')
-
-        return {
-            "loss": score,
-            "info": info
-        }
+    return {
+        "loss": score,
+        "info": info
+    }
 
 
 if __name__ == "__main__":
@@ -104,7 +73,8 @@ if __name__ == "__main__":
 
     seed_experiment(number=args.bohb_id)
 
-    res = run_bohb_parallel(id=args.bohb_id,
-                            bohb_workers=1,  # this should be set to 1, otherwise the program does not execute
-                            run_id=run_id,
-                            experiment_wrapper=ExperimentWrapper())
+    # get BOHB log directory
+    working_dir = get_working_dir(run_id)
+
+    res = compute(working_dir=working_dir, bohb_id=args.bohb_id)
+    print("FINAL: ", res)
