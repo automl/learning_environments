bec7ee4893a83b6ca454a03938bdea98af8dbffd
mrrobot <andreas.saelinger@live.com>
2021-12-02 14:48:48 +0100

    updated for logging testing

--- GTNC_evaluate_cartpole.py
+++ GTNC_evaluate_cartpole.py
@@ -12,6 +12,11 @@ from automl.bohb_optim import run_bohb_parallel
 from communicate.tcp_master_selector import start_communication_thread
 import argparse
 import os
+from experiment_helpers.exp_logging import set_logger_up
+
+import logging
+
+logger = logging.getLogger()
 
 
 def my_parse():  # --bohb_id AAA --id BBB --moab_id CCC --port DDD --min_workers EEE --number_workers FFF --mode DDD
@@ -70,17 +75,17 @@ class ExperimentWrapper():
             score = float('Inf')
             score_list = []
             error = traceback.format_exc()
-            print(error)
+            logger.info(error)
 
         info = {}
         info['error'] = str(error)
         info['score_list'] = str(score_list)
 
-        print('----------------------------')
-        print('FINAL SCORE: ' + str(score))
-        print('SCORE LIST:  ' + str(score_list))
-        print("END BOHB ITERATION")
-        print('----------------------------')
+        logger.info('----------------------------')
+        logger.info('FINAL SCORE: ' + str(score))
+        logger.info('SCORE LIST:  ' + str(score_list))
+        logger.info("END BOHB ITERATION")
+        logger.info('----------------------------')
 
         return {
             "loss": score,
@@ -93,26 +98,31 @@ def get_working_dir(run_id):
 
 
 if __name__ == "__main__":
+    args = my_parse()
+
     x = datetime.datetime.now()
     run_id = 'GTNC_evaluate_cartpole_' + x.strftime("%Y-%m-%d-%H")
 
-    args = my_parse()
+    # LOGGING:
+    set_logger_up(logger=logger, name=f"log_MASTER_id_{args.bohb_id}")
+    logger.info(f"Starting: {run_id}")
 
+    # COMMUNICATION:
     start_communication_thread(args=args)
 
     seed_experiment(number=args.bohb_id)
-    """
+
     # This is the call with BOHB:
+    """
     res = run_bohb_parallel(id=args.bohb_id,
                             bohb_workers=1,  # this should be set to 1, otherwise the program does not execute
                             run_id=run_id,
-                            experiment_wrapper=ExperimentWrapper())
+                            experiment_wrapper=ExperimentWrapper())    
     """
-    # This is the call without any BOHB:
 
+    # This is the call without any BOHB:
     exp = ExperimentWrapper()
 
     w_dir = get_working_dir(run_id)
 
     exp.compute(working_dir=w_dir, bohb_id=args.bohb_id)
-
